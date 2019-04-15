9433f779f307279759a25099ca9baeb4a04d7642
ccriston <45892724+ccriston@users.noreply.github.com>
2019-04-15 14:30:23 +0300

    Add files via upload

---
+++ Libft/ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccriston <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/02 16:42:50 by ccriston          #+#    #+#             */
+/*   Updated: 2019/04/13 19:16:06 by ccriston         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(const char *src)
+{
+	char	*dest;
+	int		i;
+	int		l;
+
+	l = 0;
+	i = 0;
+	while (src[l] != '\0')
+		l++;
+	if (!(dest = ((char *)malloc(sizeof(char) * (l + 1)))))
+		return (NULL);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
