a104a2f93b0e910248d67b9bd5c7267d8d0e2394
Clydas Criston <ccriston@oa-f5.21-school.ru>
2019-05-16 12:00:51 +0300

    fillit

---
+++ fillit/fillit.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccriston <ccriston@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/07 19:09:46 by tjuana            #+#    #+#             */
+/*   Updated: 2019/05/16 11:57:58 by ccriston         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+static void		free_map(t_game *game)
+{
+	int		i;
+	t_piece	*arr;
+
+	i = 0;
+	if (game->map)
+	{
+		while (i < 4 * game->nb_pieces)
+			ft_memdel((void **)&(game->map[i++]));
+		ft_memdel((void **)&(game->map));
+	}
+	while (!(game->pieces == NULL))
+	{
+		i = 0;
+		arr = game->pieces->next;
+		while (i < game->pieces->hight)
+			ft_memdel((void **)&(game->pieces->map[i++]));
+		ft_memdel((void **)&(game->pieces->map));
+		ft_memdel((void **)&(game->pieces));
+		game->pieces = arr;
+	}
+	ft_memdel((void **)&(game->pieces));
+}
+
+/*
+** print the valid map
+*/
+
+void			put_the_map(t_game *game)
+{
+	int i;
+	int k;
+
+	k = 0;
+	while (k < game->size)
+	{
+		i = 0;
+		while (i < game->size)
+			ft_putchar(game->map[k][i++]);
+		ft_putchar('\n');
+		k++;
+	}
+}
+
+int				main(int c, char **v)
+{
+	t_game game;
+
+	if (c != 2)
+	{
+		print_error(1);
+		return (1);
+	}
+	if (game_in(&game, v[1]))
+	{
+		solution(&game);
+		put_the_map(&game);
+		free_map(&game);
+	}
+	else
+		print_error(2);
+	return (0);
+}
---
+++ fillit/solution.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solution.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccriston <ccriston@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/10 12:58:53 by tjuana            #+#    #+#             */
+/*   Updated: 2019/05/15 15:39:14 by ccriston         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+/*
+** if the last piece doesnt work, erase it outta map
+*/
+
+static int	revert_map(t_game *game, t_piece *pic, int w, int h)
+{
+	int i;
+	int k;
+
+	k = 0;
+	w = (h > 0 ? pic->width - 1 : w);
+	while (k <= h)
+	{
+		i = 0;
+		while (i <= w)
+		{
+			if (game->map[pic->y + k][pic->x + i] == pic->letter)
+				game->map[pic->y + k][pic->x + i] = TEMPTY;
+			i++;
+		}
+		k++;
+	}
+	return (0);
+}
+
+/*
+** an attempt to put the pieces on the map
+*/
+
+static int	add_to_map(t_game *gam, t_piece *pic)
+{
+	int i;
+	int k;
+	int let_go;
+
+	k = 0;
+	let_go = 1;
+	if (gam->map[pic->y][pic->x] != TEMPTY && pic->map[0][0] != TEMPTY)
+		return (0);
+	while (let_go && k < pic->hight)
+	{
+		i = 0;
+		while (let_go && i < pic->width)
+		{
+			if (gam->map[pic->y + k][pic->x + i] != TEMPTY && \
+					pic->map[k][i] != TEMPTY)
+				let_go = 0;
+			else if (gam->map[pic->y + k][pic->x + i] == TEMPTY)
+				gam->map[pic->y + k][pic->x + i] = pic->map[k][i];
+			i = (let_go) ? i + 1 : i;
+		}
+		k = (let_go) ? k + 1 : k;
+	}
+	if (let_go)
+		return (1);
+	return (revert_map(gam, pic, i, k));
+}
+
+/*
+** check is this the answer
+*/
+
+static int	answer(t_game *game, t_piece *pic)
+{
+	if (!pic)
+		return (1);
+	pic->x = 0;
+	pic->y = 0;
+	while (pic->y + pic->hight <= game->size)
+	{
+		pic->x = 0;
+		while (pic->x + pic->width <= game->size)
+		{
+			if (add_to_map(game, pic))
+			{
+				if (answer(game, pic->next))
+					return (1);
+				else
+					revert_map(game, pic, pic->width - 1, pic->hight - 1);
+			}
+			pic->x++;
+		}
+		pic->y++;
+	}
+	return (0);
+}
+
+void		solution(t_game *game)
+{
+	int go;
+
+	while (!answer(game, game->pieces))
+	{
+		game->size++;
+		go = 0;
+		while (go < game->size)
+			ft_memset(game->map[go++], TEMPTY, game->size);
+	}
+}
